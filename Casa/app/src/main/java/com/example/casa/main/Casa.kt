a99a3c18b5080bad6cb66110fb2f646ba3ff092f
YandryJBM <130621476+CRISTHINAPA@users.noreply.github.com>
2024-05-11 21:39:07 -0500

    Practica Jet pack
    practica de jetpack y firebase

--- Casa/app/src/main/java/com/example/casa/main/Casa.kt
+++
@@ -1,8 +0,0 @@
-package com.example.casa.main
-
-data class Casa(
-    val nombre:String,
-    val descripcion: String,
-    val año: String,
-    val categoria: String,
-)
--- Casa/app/src/main/java/com/example/casa/main/MainState.kt
+++
@@ -1,9 +0,0 @@
-package com.example.casa.main
-
-data class MainState (
-    val nombre: String="",
-    val descripcion: String="",
-    val año: String="",
-    val categoria: String="",
-    val nombres: List<Casa> = emptyList()
-)
